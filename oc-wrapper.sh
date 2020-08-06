20f376b06cc5039a84d203fce0e70820cea18058
Harshad Reddy Nalla <hnalla@redhat.com>
2020-08-06 09:39:51 -0400

    :jack_o_lantern: include only necessary oc binaries
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- oc-wrapper.sh
+++ oc-wrapper.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
 case $OC_VERSION in
-    3.10|3.10+|3.10.*)
-        OC_VERSION=3.10
-        ;;
-    3.11|3.11+|3.11.*)
-        OC_VERSION=3.11
-        ;;
-    4.0|4.0+|4.0.*)
-        OC_VERSION=4.0
-        ;;
-    4.1|4.1+|4.1.*)
-        OC_VERSION=4.1
-        ;;
-    4.2|4.2+|4.2.*)
-        OC_VERSION=4.2
-        ;;
-    4.3|4.3+|4.3.*)
-        OC_VERSION=4.3
+    4.*)
+        OC_VERSION=4
         ;;
     *)
-        OC_VERSION=4.1
+        OC_VERSION=3.11
         ;;
 esac
 
