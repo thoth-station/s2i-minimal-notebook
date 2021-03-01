6bdfda87ec463924f4e4185a12966a0ab64b5db2
Sean Pryor <spryor@redhat.com>
2021-03-01 12:35:04 -0500

    Changes for Anaconda integration
    Force jupyterlab
    
    Add .condarc to limit user from using free packages
    
    Ensure notebook uses /opt/app-root env

--- start-notebook.sh
+++ start-notebook.sh
@@ -4,6 +4,12 @@ set -x
 
 set -eo pipefail
 
+APP_ROOT=${APP_ROOT:-/opt/app-root/src}
+
+source /opt/anaconda3/bin/activate ${APP_ROOT}
+
+JUPYTER_ENABLE_LAB=true
+
 if [[ ! -z "${JUPYTER_ENABLE_KERNELGATEWAY}" ]]; then
     exec /opt/app-root/bin/start-kernelgateway.sh "$@"
 fi
